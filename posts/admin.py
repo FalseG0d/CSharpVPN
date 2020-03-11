6593dcc85f449c173f7cd619742e799a3cf8bb2a
Apoorv Garg <u.garg.10@gmail.com>
2020-03-11 23:17:34 +0530

    Basic Django Blog Complete

--- posts/admin.py
+++ posts/admin.py
@@ -5,8 +5,9 @@ from .models import Post
 # Register your models here.
 
 class PostModelAdmin(admin.ModelAdmin):
-    list_display=["title","timestamp","update"]
+    list_display=["title","timestamp","update","draft","publish"]
     list_filter=["update","timestamp"]
+    list_editable = ["draft","publish"]
     search_fields=["title","content"]
     class Meta:
         model=Post
--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,25 @@
 from django.contrib import messages
 from django.shortcuts import render,get_object_or_404,redirect
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse,HttpResponseRedirect,Http404
+from django.db.models import Q
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.shortcuts import render
+from django.utils import timezone
+from urllib.parse import quote_plus
 from .forms import PostForm
 from .models import Post
 # Create your views here.
 
 def post_create(request):
+
+    if not (request.user.is_staff or request.user.is_superuser):
+        raise Http404
+
     form=PostForm(request.POST or None,request.FILES or None)
 
     if form.is_valid():
         instance=form.save(commit=False)
+        instance.user=request.user
         instance.save() 
         messages.success(request,"Successfully Created")
         return HttpResponseRedirect(instance.get_absolute_url())
@@ -24,14 +32,32 @@ def post_create(request):
 
 def post_detail(request,id=None):
     instance=get_object_or_404(Post,id=id)
+    if instance.draft or instance.publish>timezone.now().date():
+        if not request.user.is_superuser or not request.user.is_staff:
+            raise Http404
+
+    share_string=quote_plus(instance.content)
     context={
         "title":instance.title,
-        "instance":instance
+        "instance":instance,
+        "share_string":share_string,
     }
     return render(request,"post_detail.html",context)    
 
 def post_list(request):
-    queryset_list=Post.objects.all()#.order_by("-timestamp")
+    queryset_list=Post.objects.active()
+
+    if request.user.is_staff or request.user.is_superuser:
+        queryset_list=Post.objects.all()
+
+    query=request.GET.get("query")
+    if(query):
+        queryset_list=queryset_list.filter(
+            Q(title__icontains=query)|
+            Q(content__icontains=query)|
+            Q(user__first_name__icontains=query)|
+            Q(user__last_name__icontains=query)).distinct()
+
     paginator = Paginator(queryset_list, 10) # Show 10 contacts per page.
     pagereqvar='page'
     page = request.GET.get(pagereqvar)
@@ -50,6 +76,11 @@ def post_list(request):
     return render(request,"post_list.html",context)
 
 def post_update(request,id=None):
+
+    if not (request.user.is_staff or request.user.is_superuser):
+        raise Http404
+
+
     instance=get_object_or_404(Post,id=id)
     form=PostForm(request.POST or None,request.FILES or None,instance=instance)
     
