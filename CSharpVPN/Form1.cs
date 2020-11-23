5b87a87dfd6a5d97644a4188ad7e203d8114afb3
Apoorv Garg <u.garg.10@gmail.com>
2020-11-23 18:22:07 +0530

    Update Form1.cs

--- CSharpVPN/Form1.cs
+++ CSharpVPN/Form1.cs
@@ -16,24 +16,49 @@ namespace CSharpVPN
         {
             if (comboBox1.Text == "Server")
             {
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
                 if (radioButton1.Checked)
                 {
-                    Process process = new Process();
-                    ProcessStartInfo startInfo = new ProcessStartInfo();
-                    //startInfo.FileName=
-                    string path = Directory.GetCurrentDirectory();
-                    Console.WriteLine(path);
+                    startInfo.FileName = @"C:\Program Files\OpenVPN\bin\openvpn.exe";
+                    //Choose The Path To Required OpenVPN.exe file
+                    startInfo.Arguments = "--config server.ovpn";
+                    startInfo.Verb = "runas";
+
+                    process.StartInfo = startInfo;
+                    process.Start();
+                    MessageBox.Show("You Have Been Connected To the Server with UDP");
                 }
                 else if (radioButton2.Checked)
                 {
+                    startInfo.FileName = @"C:\Program Files\OpenVPN\bin\openvpn.exe";
+                    //Choose The Path To Required OpenVPN.exe file
+                    startInfo.Arguments = "--config server.ovpn";
+                    startInfo.Verb = "runas";
 
+                    process.StartInfo = startInfo;
+                    process.Start();
+                    MessageBox.Show("You Have Been Connected To the Server with FTP");
                 }
             }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            disconnect();
+            MessageBox.Show("You Have Been Disconnected From The Server.")
+        }
 
+        private void disconnect()
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName="taskkill",
+                Arguments=$"/f /im openvpn.exe",
+                CreateNoWindow=true,
+                UseShellExecute=false
+            }).WaitForExit();
         }
     }
 }
